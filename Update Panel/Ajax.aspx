67e57f278ff0fe84d72d5e19975a213cd7cf1847
Ashokkumaresan <ashok1989.stone@gmail.com>
2015-10-09 17:25:33 +0530

    Update Panel Sample
    Update Panel Sample

---
+++ Update Panel/Ajax.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Ajax.aspx.cs" Inherits="Ajax" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server" id="head1">
+    <title>Update Panel Sample</title>
+    <script src="fusioncharts/jquery-1.11.0.js"></script>
+    <script type="text/javascript" src="fusioncharts/fusioncharts.js"></script>
+    <script type="text/javascript">     
+        function chartMethod(p_chartid, p_charttype, p_width, p_height, p_datasource, p_renderid) {
+            // Recreate render div and append  fusion script to body tag;            
+            $('body').append('<script type="text/javascript">FusionCharts && FusionCharts.ready(function () {if (FusionCharts("' + p_chartid + '") ) FusionCharts("' + p_chartid + '").dispose();    var chart_myChart = new FusionCharts({        "id" : "' + p_chartid + '",         "width" : "' + p_width + '",         "dataSource" : "' + p_datasource + '",         "type" : "' + p_charttype + '",         "height" : "' + p_height + '",         "renderAt" : "' + p_renderid + '",         "dataFormat" : "jsonurl",     }).render();});');
+        }
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <label id="hfHiddenFieldID"></label>
+            <asp:ScriptManager ID="ScriptManager1" runat="server">
+            </asp:ScriptManager>
+
+        </div>
+        <asp:UpdatePanel ID="UpdatePanel1" runat="server" UpdateMode="Conditional" 
+                ChildrenAsTriggers="true">
+            <ContentTemplate>
+                <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Post Back" />
+                <br />
+                <asp:Label ID="Label1" runat="server" Text="Label1"></asp:Label>
+            </ContentTemplate>
+            <Triggers>
+     <asp:AsyncPostBackTrigger ControlID="Button1" EventName="Click"/>
+</Triggers>
+        </asp:UpdatePanel>      
+    </form>
+</body>
+</html>
---
+++ Update Panel/Ajax.aspx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using FusionCharts.Charts;
+
+public partial class Ajax : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            Chart sales = new Chart();
+            sales.SetChartParameter(Chart.ChartParameter.chartId, "myChart");
+            string param = sales.GetChartParameter(Chart.ChartParameter.chartId);
+
+            // Setting chart type to Column 3D chart
+            sales.SetChartParameter(Chart.ChartParameter.chartType, "column3d");
+
+            // Setting chart width to 500px
+            sales.SetChartParameter(Chart.ChartParameter.chartWidth, "600");
+
+            // Setting chart height to 400px
+            sales.SetChartParameter(Chart.ChartParameter.chartHeight, "350");
+
+            // Setting chart data as XML URL
+            sales.SetData("Data/Data.aspx", Chart.DataFormat.xmlurl);
+         
+            Label1.Text = sales.Render();
+
+        }       
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        chartproperty chprop = new chartproperty();
+        chprop.chartId = "myChart1";
+        chprop.chartType = "column3d";
+        chprop.chartWidth = "600";
+        chprop.chartHeight = "350";
+        chprop.xmlurl = "Data/maindata.json";
+        chprop.renderId = "Label1";
+        List<chartproperty> listofchart = new List<chartproperty>();
+        listofchart.Add(chprop);       
+        foreach (var item in listofchart.Select((x, i) => new { Value = x, Index = i }))
+        {
+            // calling javacript function passing all chart properties.
+           ScriptManager.RegisterStartupScript(UpdatePanel1, UpdatePanel1.GetType(), "codeMethod" + item.Index + "", "chartMethod('" + item.Value.chartId + "','" + item.Value.chartType + "','" + item.Value.chartWidth + "','" + item.Value.chartHeight + "','" + item.Value.xmlurl + "','" + item.Value.renderId + "');", true);           
+        }        
+    }  
+    public class chartproperty  // Chart Properties
+    {
+        public string chartId { get; set; }
+        public string chartType { get; set; }
+        public string xmlurl { get; set; }
+        public string chartWidth { get; set; }
+        public string chartHeight { get; set; }
+        public string renderId { get; set; }
+    }
+}
